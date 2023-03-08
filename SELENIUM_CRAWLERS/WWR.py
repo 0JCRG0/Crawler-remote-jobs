5a4369a23c4feefec8db129bb06be41d29548c19
JCRG <juancarlosrg1999@gmail.com>
2023-03-08 00:22:41 +0000

    WWR ready

--- SELENIUM_CRAWLERS/WWR.py
+++ SELENIUM_CRAWLERS/WWR.py
@@ -1,7 +1,5 @@
 #! python3
 # searchpypi.py  - Opens several search results.
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import requests
 import bs4
 import sys
@@ -10,51 +8,32 @@ import re
 import pretty_errors
 
 def WWR():
-    # CRITICAL!!! -> UNCOMMENT URL DEPENDING ON YOUR NEEDS...
-    
-    #url = sales_url = 'https://weworkremotely.com/remote-jobs/search?term=&categories%5B%5D=9&region%5B%5D=0&region%5B%5D=4&job_listing_type%5B%5D=Contract&job_listing_type%5B%5D=Full-Time'
-    #url = software_eng_url = 'https://weworkremotely.com/remote-jobs/search?term=&categories%5B%5D=17&categories%5B%5D=18&categories%5B%5D=4&region%5B%5D=0&region%5B%5D=4&job_listing_type%5B%5D=Full-Time'
-    #url = python_url = 'https://weworkremotely.com/remote-jobs/search?term=Python&button=&categories%5B%5D=17&categories%5B%5D=18&categories%5B%5D=4&region%5B%5D=0&region%5B%5D=4&job_listing_type%5B%5D=Full-Time'
-    url = all_jobs = 'https://weworkremotely.com/remote-full-time-jobs'
-
-    #num_pages = 6
-
-
-    print('Searching...')
-    def make_soup(url):
-        try:
-            res = requests.get(url)
-            res.raise_for_status()
-        except Exception as e:
-            print(f'An error occurred: {e}')
-            sys.exit(1)
-        return res
-
-
-    soup = bs4.BeautifulSoup(make_soup(url).text, 'html.parser')
-
-    #Getting every URL
-    """
-    def URL():
-        urls_list = []
-        curated_url = []
-        all_url = soup.find_all(href=re.compile("/remote-jobs/|/listings/")) #We use | to include both href
-        rubish = ['https://weworkremotely.com/remote-jobs/search', 'https://weworkremotely.com/remote-jobs/new', 'https://weworkremotely.comhttps://weworkremotely.com/remote-jobs/search']
-        for link in all_url:
-            href = link.get('href')
-            urls_list.append("https://weworkremotely.com" + href)
-        curated_url = [url for url in urls_list if url not in rubish]
-        return curated_url
-    """
-
-    def READY_JOBS():
-        jobs = []
-        links_dirty = [] #links not curated
-        links_all = []
-        titles_all = []
-        locations_all = []
-        pubdates_all = []
-        rubish = ['https://weworkremotely.com/remote-jobs/search', 'https://weworkremotely.com/remote-jobs/new', 'https://weworkremotely.comhttps://weworkremotely.com/remote-jobs/search']
+    num_pages = 6
+    jobs = []
+    links_dirty = [] #links not curated
+    links_all = []
+    titles_all = []
+    locations_all = []
+    pubdates_all = []
+    rubish = ['https://weworkremotely.com/remote-jobs/search', 'https://weworkremotely.com/remote-jobs/new', 'https://weworkremotely.comhttps://weworkremotely.com/remote-jobs/search']
+        
+    #Iterate through all the pages
+    for i in range(1, num_pages + 1):
+        url = f"https://weworkremotely.com/remote-full-time-jobs?page={i}"   
+            
+        #Make the soup...
+        print('Crawling...' + url)
+        def make_soup(url):
+            try:
+                res = requests.get(url)
+                res.raise_for_status()
+            except Exception as e:
+                print(f'An error occurred: {e}')
+                sys.exit(1)
+            return res
+        soup = bs4.BeautifulSoup(make_soup(url).text, 'html.parser')
+            
+        #Get the elements...       
         titles_parsed = soup.select('.title') #getting the class
         locations_parsed = soup.select('.region.company') #getting the class
         pubdates_parsed = soup.find_all('time') #getting the tag
@@ -70,17 +49,11 @@ def WWR():
             location = l.get_text()
             locations_all.append(location)
         for p in pubdates_parsed:
-            pubdate = p.get('datetime')
+            pubdate = p.get('datetime') #getting attrs
             pubdates_all.append(pubdate)
-        jobs = {"title": titles_all, "link": links_all, "pubdate": pubdates_all, "location": locations_all, "description": " "}
-        return jobs
-
-    data = READY_JOBS()
-    data_dic = dict(data)
-    df = pd.DataFrame.from_dict(data_dic, orient='index')
-    df = df.transpose()
-
-    print(df)
+        jobs += [{"title": title, "link": link, "pubdate": pubdate, "location": location, "description": " "} for title, link, pubdate, location in zip(titles_all, links_all, pubdates_all, locations_all)]
+    #PARSE IT TO A PANDAS DF
+    df = pd.DataFrame.from_records(jobs)
+    #VERIFY THE DATA...
     df.to_csv("WWR.csv", index=False)
-
 WWR()
