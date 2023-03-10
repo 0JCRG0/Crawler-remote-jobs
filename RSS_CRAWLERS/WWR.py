e2b0488459d6ecc9e8af4f77755baee21f5fec28
JCRG <juancarlosrg1999@gmail.com>
2023-03-10 22:43:58 +0000

    added postgresql

--- RSS_CRAWLERS/WWR.py
+++ RSS_CRAWLERS/WWR.py
@@ -5,9 +5,11 @@ import bs4
 import sys
 import pandas as pd
 import re
+import psycopg2
 import pretty_errors
 
 def WWR():
+    print("\n", "Crawler deployed... ", "\n")
     num_pages = 6
     jobs = []
     links_dirty = [] #links not curated
@@ -54,7 +56,68 @@ def WWR():
         jobs += [{"title": title, "link": link, "pubdate": pubdate, "location": location, "description": " "} for title, link, pubdate, location in zip(titles_all, links_all, pubdates_all, locations_all)]
     #PARSE IT TO A PANDAS DF
     df = pd.DataFrame.from_records(jobs)
+    
+    #COUNT THE JOBS 
+    print("\n", f"Crawler successfully found {len(df)} jobs...", "\n")
+    
     #SAVE DATA...
+    print("\n", "Saving jobs in local machine as a CSV file...", "\n")
     directory = "./OUTPUTS/"
     df.to_csv(f"{directory}WWR.csv", index=False)
+    
+    #SEND IT TO TO PostgreSQL
+    print("\n", f"Fetching {len(df)} jobs to PostgreSQL...", "\n")
+
+    # This code creates a new table per iteration
+    ## Create a connection to the PostgreSQL database
+    cnx = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
+
+    # create a cursor object
+    cursor = cnx.cursor()
+
+    # Get the name of the next table to create
+    get_table_name_query = '''
+        SELECT COUNT(*) FROM information_schema.tables
+        WHERE table_name LIKE 'WWR_%'
+    '''
+    cursor.execute(get_table_name_query)
+    # execute the query to get the count of existing tables
+
+    # fetch the first row of the query results
+    result = cursor.fetchone()
+
+    # get the number of existing tables if there are any, otherwise set it to 0
+    next_table_number = result[0] + 1 if result is not None else 0
+    next_table_name = 'WWR_{}'.format(next_table_number)
+
+    # prepare the SQL query to create a new table
+    create_table_query = '''
+        CREATE TABLE {} (
+            title VARCHAR(255),
+            link VARCHAR(255),
+            description VARCHAR(1000),
+            pubdate TIMESTAMP,
+            location VARCHAR(255)
+        )
+    '''.format(next_table_name)
+
+    # execute the create table query
+    cursor.execute(create_table_query)
+
+    # insert the DataFrame into the PostgreSQL database as a new table
+    for index, row in df.iterrows():
+        insert_query = '''
+            INSERT INTO {} (title, link, description, pubdate, location)
+            VALUES (%s, %s, %s, %s, %s)
+        '''.format(next_table_name)
+        values = (row['title'], row['link'], row['description'], row['pubdate'], row['location'])
+        cursor.execute(insert_query, values)
+
+    # commit the changes to the database
+    cnx.commit()
+
+    # close the cursor and connection
+    cursor.close()
+    cnx.close()
+    print("\n", "ALL DONE! GO TO POSTGRESQL FOR FURTHER PROCESSING :)", "\n")
 WWR()
