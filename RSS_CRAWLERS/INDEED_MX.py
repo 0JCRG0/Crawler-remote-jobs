318a554adf2452bbf74eb7c08afde2fad7a5d0f8
JCRG <juancarlosrg1999@gmail.com>
2023-03-13 21:09:12 +0000

    fixed pubdate

--- RSS_CRAWLERS/INDEED_MX.py
+++ RSS_CRAWLERS/INDEED_MX.py
@@ -1,6 +1,7 @@
 #! python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 import re
 import csv
@@ -9,6 +10,7 @@ import psycopg2
 import numpy as np
 from dateutil.relativedelta import relativedelta
 from dateutil.parser import parse
+from datetime import datetime, timedelta
 
 
 # 1 TODO: FIX DATETIME (see below)
@@ -17,16 +19,33 @@ from dateutil.parser import parse
 
 "https://mx.indeed.com/jobs?q=PYTHON&sc=0kf%3Aattr%28DSQF7%29%3B&sort=date&fromage=7&filter=0&start=80&pp=gQB4AAABhth9DdoAAAAB-_3ALQAJAQA_oyaWY9L6AAA&vjk=a7a2ee2d2741c607"
 "https://mx.indeed.com/jobs?q=PYTHON&sc=0kf%3Aattr%28DSQF7%29%3B&sort=date&fromage=7&filter=0&start=70&pp=gQBpAAABhth9DdoAAAAB-_3ALQAYAQACCYx8rKQbcGPqdw-3E24IbS_nb3zaAAA&vjk=3954cde2ef9483b9"
+'https://indeed.com/jobs?q=DATA+ANALYST&sc=0kf%3Aattr%28DSQF7%29%3B&sort=date&fromage=7&filter=0&start=0'
+
 
 def indeed():
     # Start the session
     driver = webdriver.Firefox()
 
     # set the number of pages you want to scrape
-    num_pages = 10
+    num_pages = 1
+
+    # cleansing function
+    def bye_regex(s):
+        # Remove leading/trailing white space
+        s = s.strip()
+        # Replace multiple spaces with a single space
+        s = re.sub(r'\s+', ' ', s)
+        # Remove newline characters
+        s = re.sub(r'\n', '', s)
+        # Replace regex for í
+        s = re.sub(r'√≠', 'í', s)
+        # Replace word
+        s = re.sub(r'Posted', '', s)
+
+        return s
         
     # START CRAWLING
-    def crawling():
+    def crawling(keyword):
         print("\n", f"Crawler deployed... ", "\n")
         total_urls = []
         total_titles = []
@@ -35,7 +54,7 @@ def indeed():
         total_descriptions = []
         rows = []
         for i in range(0, num_pages * 10, 10):
-            url = f"https://mx.indeed.com/jobs?q=PYTHON&sc=0kf%3Aattr%28DSQF7%29%3B&sort=date&fromage=7&filter=0&start={i}"
+            url = f"https://mx.indeed.com/jobs?q={keyword}&sc=0kf%3Aattr%28DSQF7%29%3B&sort=date&fromage=7&filter=0&start={i}"
             #Make the request
             driver.get(url)
             print(f"Crawling... {url}")
@@ -56,9 +75,9 @@ def indeed():
                 href = li.get_attribute("href")
                 total_urls.append(href)
             for p in pubdates:
-                pubdate = p.text
+                dirty_pubdate = p.text
+                pubdate = bye_regex(dirty_pubdate)
                 total_pubdates.append(pubdate)
-                #TODO: ADD def clean():
             for l in locations:
                 location = l.text
                 total_locations.append(location)
@@ -68,7 +87,7 @@ def indeed():
             rows = {'titles': total_titles, 'links': total_urls, 'pubdate': total_pubdates, 'location': total_locations, 'description': ""}
         return rows
     
-    data = crawling()
+    data = crawling("DATA+ANALYST") #THIS IS YOUR SEARCH TERM - use "+" if more than 1 word
 
     
     driver.close()
@@ -91,9 +110,8 @@ if __name__ == "__main__":
 
 
 # 1 TODO: FIX DATETIME
-"""
-from datetime import datetime, timedelta
 
+"""
 # get today's datetime
 today = datetime.now()
 
@@ -109,11 +127,4 @@ yesterday = today - timedelta(days=num_days)
 # example if statement
 if str_date == "Publicado hace 1 días":
     print(f"Yesterday was {yesterday}")
-
-<span class="visually-hidden">Posted</span>"Publicado hace 9 días
-
-            pubdates = driver.find_elements(By.CSS_SELECTOR, '.date .visually-hidden')
-            for p in pubdates:
-                pubdate = p.text
-                total_pubdates.append(pubdate)
 """
