28978c1e6a67702e7299008835409133b872bda8
JCRG <juancarlosrg1999@gmail.com>
2023-03-22 21:31:57 +0000

    it works

--- DRAFTS/SCJN.py
+++ DRAFTS/SCJN.py
@@ -2,21 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
-import random
-import time
 import pandas as pd
 import re
 import pretty_errors
-import csv
-import psycopg2
 import numpy as np
 import bs4
 import json
 import requests
-import sys
 import timeit
+from clean_regex import bye_regex, pandas_regex
 
 
 def scjn():
@@ -28,10 +22,10 @@ def scjn():
     driver = webdriver.Firefox()
 
     # set the number of pages you want to scrape
-    num_pages = 3
+    num_pages = 1
 
     def url_araña():
-        print("\n", f"Iniciando araña... 0%", "\n")
+        print("\n", f"ARAÑA 1 activa...", "\n")
         urls = []
         total_urls = []
 
@@ -39,7 +33,7 @@ def scjn():
             url = f"https://bj.scjn.gob.mx/busqueda?q=*&indice=tesis&subFiltros=tipo.keyword:Tesis%20de%20Jurisprudencia,materia.keyword:Laboral&size=25&page={i}"
             #Make the request
             #Make the soup...
-            print("\n", 'Tejiendo telaraña en...' + url, "\n")
+            print("\n", f'Obteniendo urls en ---> {url}', "\n")
             #Make the request
             driver.get(url)
             #Wait until the elements appear
@@ -53,23 +47,96 @@ def scjn():
                 total_urls = {"link": urls}
         return total_urls
     
-    data_ = url_araña()
+    urls = url_araña()
 
     #close the driver
     driver.close()
 
-    data = dict(data_)
+    data = dict(urls)
     df = pd.DataFrame.from_dict(data, orient='index')
     df = df.transpose()
-    print(df)
 
-    print("\n", "Guardando tesis en archivos locales...", "\n")
+    print("\n", "Guardando urls en archivos locales...", "\n")
     directory = "./OUTPUTS/"
-    df.to_csv(f'{directory}SCJN_LABORAL_JURISPRUDENCIA.csv', index=False)
+    df.to_csv(f'{directory}urls_scjn_laboral_jurisprudencia.csv', index=False)
 
 #print the time
     elapsed_time = timeit.default_timer() - start_time
-    print("\n", f"Terminó la araña en: {elapsed_time:.5f} segundos", "\n")
+    print("\n", f"ARAÑA 1 encontró {len(df)} urls en: {elapsed_time:.1f} segundos", "\n")
+
+    print("\n", "ARAÑA 2 activa.", "\n")
+    driver1 = webdriver.Firefox()
+    def iterate_df():
+        #Start the timer
+        start_time = timeit.default_timer()
+        #columns...
+        total_ids = []
+        total_titulos = []
+        total_contenidos = []
+        total_notas = []
+        #df
+        data = []
+        for index, row in df.iterrows():
+            link = row['link']
+            driver1.get(link)
+            print("\n", f"Obteniendo todo el texto de ---> {link}", "\n")
+
+            #Identifica los elementos
+            id = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#cont-principal .int-doc-tesis.ng-star-inserted .int-doc-tesis__metadata.row.p-2.m-0')))
+            titulo = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#cont-principal .int-doc-tesis.ng-star-inserted .int-doc-tesis__information')))
+            contenido = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#cont-principal .int-doc-tesis.ng-star-inserted .int-doc-tesis__content')))
+            notas = WebDriverWait(driver1, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#cont-principal .int-doc-tesis.ng-star-inserted .int-doc-tesis__notas')))
+            
+            #Obten el texto de id
+            id_sucio = id.get_attribute("innerHTML")
+            #limpia 
+            id = bye_regex(id_sucio)
+            #agregalo
+            total_ids.append(id)
+            
+            #Obten el texto de titulos
+            titulo_sucio = titulo.get_attribute("innerHTML")
+            #limpia
+            titulo = bye_regex(titulo_sucio)
+            #agregalo
+            total_titulos.append(titulo)
+            
+            #Obten todos los contenidos
+            contenido_sucio = contenido.get_attribute("innerHTML")
+            #limpia
+            contenido = bye_regex(contenido_sucio)
+            #agregalo
+            total_contenidos.append(contenido)
+
+            #Obten todas las notas
+            notas_sucio = notas.get_attribute("innerHTML")
+            #limpia
+            notas_limpio = bye_regex(notas_sucio)
+            #agregalo
+            total_notas.append(notas_limpio)
+
+            data = {'ID': total_ids, 'TITULO': total_titulos, 'CONTENIDO': total_contenidos, 'NOTAS':total_notas}
+        return data
+    
+    data = iterate_df()
+    driver1.close()
+    
+    #Convert data to a pandas df for further analysis
+    print("\n", "Convirtiendo en df...", "\n")
+    data_dic = dict(data)
+    df1 = pd.DataFrame.from_dict(data_dic, orient='index')
+    df1 = df1.transpose()
+
+    #apply regex to every letter of the df
+    df1 = df.apply(pandas_regex, axis=0)
+
+    print("\n", "Guardando tesis en archivos locales...", "\n")
+    directory = "./OUTPUTS/"
+    df1.to_csv(f'{directory}scjn_laboral_jurisprudencias.csv', index=False)
+
+    #print the time
+    elapsed_time = timeit.default_timer() - start_time
+    print("\n", f"ARAÑA 2 obtuvo {len(df1)} jurisprudencias en: {elapsed_time:.1f} segundos", "\n")
 
 
 if __name__ == "__main__":
