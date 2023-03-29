0c380ab3e4d4173bf6879c8778b726b5d7c93e89
JCRG <juancarlosrg1999@gmail.com>
2023-03-30 00:02:27 +0100

    added postgre to himalayas

---
+++ SELENIUM_CRAWLERS/HIMALAYAS_exp.py
@@ -0,0 +1,136 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import re
+import csv
+import pretty_errors
+import psycopg2
+import timeit
+from dateutil.relativedelta import relativedelta
+from utils.handy import clean_rows, initial_clean, test_postgre, send_postgre
+
+#TODO: FIX POSTGRESQL SO THE TRIGGER RUNS THE FUNCTIONS WHEN A TABLE STARTING WITH ® IS ADDED
+
+def HIMALAYAS():
+    #start timer
+    start_time = timeit.default_timer()
+
+    def CRAWLER_HIMALAYAS():
+        
+        # Start the session
+        driver = webdriver.Firefox()
+
+        # set the number of pages you want to scrape
+        num_pages = 6
+
+        # START CRAWLING
+        def CRAWLING():
+            print("\n", f"Crawler deployed... ", "\n")
+            total_urls = []
+            total_titles = []
+            total_pubDates = []
+            total_locations = [] 
+            total_categories = []
+            rows = []
+            for i in range(1, num_pages + 1):
+                url = f"https://himalayas.app/jobs?page={i}"
+                driver.get(url)
+                print(f"Crawling... {url}")
+                # Establish Waiting Strategy
+                driver.implicitly_wait(1)
+                #GETTING THE PARENT...
+                jobs = driver.find_elements(By.CSS_SELECTOR, '#card-group [name = "card"]')
+                for job in jobs:
+                    #GETTING THE ELEMENTS FROM THE CHILDREN... remember to use css_selectors instead of tags
+                    all_urls = job.find_elements(By.CSS_SELECTOR, ' .mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .flex.flex-row.items-center a')
+                    all_titles = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .flex.flex-row.items-center a .text-xl.font-medium.text-gray-900')
+                    all_pubDates = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .hidden.md\:block.md\:whitespace-nowrap.md\:ml-2.relative div .text-base.text-gray-600')
+                    all_location = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-center.justify-between .flex.flex-row.items-center.flex-wrap.overflow-hidden .hidden.md\:flex.md\:flex-row.md\:items-center .badge.badge-gray.no-hover.mr-2 .flex.flex-row.items-center .badge-text.whitespace-nowrap.no-hover.text-center')
+                    all_category = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-center.justify-between .flex.flex-row.items-center.flex-wrap.overflow-hidden .flex div .flex.flex-row.items-center .badge-text.text-center.whitespace-nowrap')
+                    for url in all_urls:
+                        href = url.get_attribute("href")
+                        total_urls.append(href)
+                    for titl in all_titles:
+                        title = titl.get_attribute("innerHTML")
+                        curated_title = [initial_clean(title)] #We clean the title with the function in handy
+                        total_titles.append(curated_title)
+                    for dates in all_pubDates:
+                        pubDate = dates.get_attribute("innerHTML") 
+                        total_pubDates.append(pubDate)
+                    for loc in all_location:
+                        location = loc.get_attribute("innerHTML") 
+                        total_locations.append(location)
+                    for categ in all_category:
+                        category = categ.get_attribute("innerHTML") 
+                        total_categories.append(category)
+                    #Save it all in a single list of dictionaries
+                    rows = {"title": total_titles, "link": total_urls, "pubdate": total_pubDates, "location": total_locations, "description": total_categories}
+            return rows
+        #Quit the driver
+        data = CRAWLING()
+        driver.quit()
+        
+        #The data had missing values so we have to do this to convert it into a pandas df
+        print("\n", "Converting data to a pandas df...", "\n")
+        data_dic = dict(data)
+        df = pd.DataFrame.from_dict(data_dic, orient='index')
+        df = df.transpose()
+        print("\n", f"Crawler successfully found {len(df)} jobs...", "\n")
+
+
+        #Save it in local machine
+        print("\n", "Saving jobs in local machine as a CSV file...", "\n")
+        directory = "./OUTPUTS/"
+        df.to_csv(f"{directory}himalaya.csv", index=False)
+        return df
+
+    def PIPELINE():
+        df = CRAWLER_HIMALAYAS()
+        print("\n", "Processing of the df has started...", "\n")
+
+        # Fill missing values with "NaN"
+        df.fillna("NaN", inplace=True)
+
+        # Mask to convert minutes|hour% to 1 day ago so it can be parse to date time (cba to say it was posted today, maybe later should be fixed)
+        mask = df['pubdate'].str.contains('minutes|hour%')
+        df.loc[mask, 'pubdate'] = "1 day ago"
+
+        # From relative date strings to date time...
+        def convert_date(x):
+            if isinstance(x, str) and 'ago' in x:
+                return pd.Timestamp.today() - relativedelta(days=int(x.split()[0]))
+            return x
+
+        # Convert str to datetime & clean titles
+        for col in df.columns:
+            if col == 'pubdate':
+                df[col] = df[col].apply(convert_date).astype('datetime64[ns]')
+            if col == 'title':
+                df[col] = df[col].astype(str)
+                df[col] = df[col].apply(clean_rows)
+
+        ##reindex
+        df = df.reindex(columns=['title', 'link', 'description', 'pubdate', 'location'])
+
+        # Filter rows by a date range (this reduces the number of rows... duh)
+        ## set the date range
+        start_date = pd.to_datetime('2016-01-01')
+        end_date = pd.to_datetime('2023-02-15')
+        ## apply it
+        date_range_filter = (df['pubdate'] >= start_date) & (df['pubdate'] <= end_date)
+        df = df.loc[~date_range_filter]
+
+        # SEND IT TO TO PostgreSQL
+        print("\n", f"Fetching {len(df)} jobs to PostgreSQL...", "\n")
+
+        send_postgre(df)
+        
+        #print the time
+        elapsed_time = timeit.default_timer() - start_time
+        print("\n", f"All done! {len(df)} jobs were found, cleaned, reformatted, filtered and sent to PostgreSQL in: {elapsed_time:.2f} seconds", "\n")
+
+    PIPELINE()
+
+
+if __name__ == "__main__":
+    HIMALAYAS()
--- SELENIUM_CRAWLERS/utils/handy.py
+++ SELENIUM_CRAWLERS/utils/handy.py
@@ -1,4 +1,5 @@
 import re
+import psycopg2
 
 def clean_rows(s):
     if not isinstance(s, str):
@@ -29,3 +30,115 @@ def bye_regex(s):
     s = re.sub(r'Posted', '', s)
         # Remove HTML tags
     s = re.sub(r'<.*?>', '', s)
+
+def send_postgre(df):
+        # create a connection to the PostgreSQL database
+        cnx = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
+
+        # create a cursor object
+        cursor = cnx.cursor()
+
+        # prepare the SQL query to create a new table
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS master_jobs (
+                title VARCHAR(255),
+                link VARCHAR(255) PRIMARY KEY,
+                description VARCHAR(1000),
+                pubdate TIMESTAMP,
+                location VARCHAR(255)
+            )
+        '''
+
+        # execute the create table query
+        cursor.execute(create_table_query)
+
+        print("\n", "Inserting jobs into PostgreSQL using an upsert strategy...", "\n")
+
+        # insert the DataFrame into the PostgreSQL database using an upsert strategy
+        for index, row in df.iterrows():
+            insert_query = '''
+                INSERT INTO master_jobs (title, link, description, pubdate, location)
+                VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (link) DO UPDATE SET
+                    title = excluded.title,
+                    description = excluded.description,
+                    pubdate = excluded.pubdate,
+                    location = excluded.location
+            '''
+            values = (row['title'], row['link'], row['description'], row['pubdate'], row['location'])
+            cursor.execute(insert_query, values)
+
+        count_query = '''
+            SELECT COUNT(*) FROM master_jobs
+        '''
+        # execute the count query and retrieve the result
+        cursor.execute(count_query)
+        result = cursor.fetchone()
+
+        # check if the result set is not empty
+        if result is not None:
+            count = result[0]
+            print("\n", "DONE.", "\n", "\n", f"Current total count of jobs in PostgreSQL: {count}", "\n")
+        
+        # commit the changes to the database
+        cnx.commit()
+
+        # close the cursor and connection
+        cursor.close()
+        cnx.close()
+
+def test_postgre(df):
+        # create a connection to the PostgreSQL database
+        cnx = psycopg2.connect(user='postgres', password='3312', host='localhost', port='5432', database='postgres')
+
+        # create a cursor object
+        cursor = cnx.cursor()
+
+        # prepare the SQL query to create a new table
+        create_table_query = '''
+            CREATE TABLE IF NOT EXISTS test (
+                title VARCHAR(255),
+                link VARCHAR(255) PRIMARY KEY,
+                description VARCHAR(1000),
+                pubdate TIMESTAMP,
+                location VARCHAR(255)
+            )
+        '''
+
+        # execute the create table query
+        cursor.execute(create_table_query)
+
+        print("\n", "Inserting jobs into PostgreSQL using an upsert strategy...", "\n")
+
+        # insert the DataFrame into the PostgreSQL database using an upsert strategy
+        for index, row in df.iterrows():
+            insert_query = '''
+                INSERT INTO test (title, link, description, pubdate, location)
+                VALUES (%s, %s, %s, %s, %s)
+                ON CONFLICT (link) DO UPDATE SET
+                    title = excluded.title,
+                    description = excluded.description,
+                    pubdate = excluded.pubdate,
+                    location = excluded.location
+            '''
+            values = (row['title'], row['link'], row['description'], row['pubdate'], row['location'])
+            cursor.execute(insert_query, values)
+
+        count_query = '''
+            SELECT COUNT(*) FROM test
+        '''
+        # execute the count query and retrieve the result
+        cursor.execute(count_query)
+        result = cursor.fetchone()
+
+        # check if the result set is not empty
+        if result is not None:
+            count = result[0]
+            print("\n", "DONE.", "\n", "\n", f"Current total count of jobs in PostgreSQL: {count}", "\n")
+        
+        # commit the changes to the database
+        cnx.commit()
+
+        # close the cursor and connection
+        cursor.close()
+        cnx.close()
