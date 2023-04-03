a9b41375b762fa8a01ab0c5837930a20aec951b7
JCRG <juancarlosrg1999@gmail.com>
2023-04-03 20:34:26 +0100

    new selenium crawler template

--- selenium_resources/experiment.py
+++ selenium_resources/experiment.py
@@ -8,124 +8,114 @@ from dateutil.parser import parse
 from datetime import datetime, timedelta, date
 import json
 from dateutil.relativedelta import relativedelta
-from utils.handy import clean_rows, initial_clean, to_postgre
+from utils.handy import clean_rows, initial_clean, to_postgre, test_postgre
 
 
 
-def himalayas(pages, cut_off):
+def selenium_crawlers(pages):
     print("\n", "HIMALAYAS starting now.", "\n")
 
     #start timer
     start_time = timeit.default_timer()
 
-    def crawler():
-        #Modify the options so it is headless - to disable just comment the next 2 lines and use the commented driver
-        options = webdriver.FirefoxOptions()
-        options.add_argument('-headless')
+    #Modify the options so it is headless - to disable just comment the next 2 lines and use the commented driver
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
         
-        # Start the session
-        #driver = webdriver.Firefox()
-        driver = webdriver.Firefox(options=options)
+    # Start the session
+    #driver = webdriver.Firefox()
+    driver = webdriver.Firefox(options=options)
 
-        # START CRAWLING
-        def elements():
-            total_urls = []
-            total_titles = []
-            total_pubDates = []
-            total_locations = [] 
-            total_categories = []
-            rows = []
-            for i in range(1, pages + 1):
-                url = f"https://himalayas.app/jobs?page={i}"
-                driver.get(url)
-                print(f"Crawling... {url}")
-                # Establish Waiting Strategy
-                driver.implicitly_wait(1)
-                #GETTING THE PARENT...
-                jobs = driver.find_elements(By.CSS_SELECTOR, '#card-group [name = "card"]')
-                for job in jobs:
-                    #GETTING THE ELEMENTS FROM THE CHILDREN... remember to use css_selectors instead of tags
-                    all_urls = job.find_elements(By.CSS_SELECTOR, ' .mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .flex.flex-row.items-center a')
-                    all_titles = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .flex.flex-row.items-center a .text-xl.font-medium.text-gray-900')
-                    all_pubDates = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-start.justify-between.mb-2.w-full .hidden.md\:block.md\:whitespace-nowrap.md\:ml-2.relative div .text-base.text-gray-600')
-                    all_location = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-center.justify-between .flex.flex-row.items-center.flex-wrap.overflow-hidden .hidden.md\:flex.md\:flex-row.md\:items-center .badge.badge-gray.no-hover.mr-2 .flex.flex-row.items-center .badge-text.whitespace-nowrap.no-hover.text-center')
-                    all_category = job.find_elements(By.CSS_SELECTOR, '.mt-4.md\:mt-0.ml-0.md\:ml-5.w-full .flex.flex-row.items-center.justify-between .flex.flex-row.items-center.flex-wrap.overflow-hidden .flex div .flex.flex-row.items-center .badge-text.text-center.whitespace-nowrap')
-                    for i in all_urls:
-                        href = i.get_attribute("href")
-                        total_urls.append(href)
-                    for i in all_titles:
-                        title = i.get_attribute("innerHTML")
-                        curated_title = [initial_clean(title)] #We clean the title with the function in handy
-                        total_titles.append(curated_title)
-                    for i in all_pubDates:
-                        pubDate = i.get_attribute("innerHTML") 
-                        total_pubDates.append(pubDate)
-                    for i in all_location:
-                        location = i.get_attribute("innerHTML") 
-                        total_locations.append(location)
-                    for i in all_category:
-                        category = i.get_attribute("innerHTML") 
-                        total_categories.append(category)
-                    #Save it all in a single list of dictionaries
-                    rows = {"title": total_titles, "link": total_urls, "pubdate": total_pubDates, "location": total_locations, "description": total_categories}
-            return rows
-        #Quit the driver
-        data = elements()
-        driver.quit()
+    def elements():
+        total_urls = []
+        total_titles = []
+        total_pubdates = []
+        total_locations = [] 
+        total_descriptions = []
+        rows = []
+        with open('./selenium_resources/main_elements.json') as f:
+            #load the json
+            data = json.load(f)
+            # Access the 'urls' list in the first dictionary of the 'data' list and assign it to the variable 'urls'
+            urls = data[0]["urls"]
+            for url_obj in urls:
+                # Extract the 'url' key from the current dictionary and assign it to the variable 'url_prefix'
+                url_prefix = url_obj['url']
+                # Extract the first dictionary from the 'elements_path' list in the current dictionary and assign it to the variable 'elements_path'
+                elements_path = url_obj['elements_path'][0]
+                # +1 is done due to the exclusive nature of range
+                for i in range(1, pages + 1):
+                    url = url_prefix + str(i)
+                    # get the url
+                    driver.get(url)
+                    print(f"Crawling... {url}")
+                    # establish waiting strategy
+                    driver.implicitly_wait(1)
+                    # GETTING THE PARENT...
+                    jobs = driver.find_elements(By.CSS_SELECTOR, elements_path["jobs_path"])
+                    for job in jobs:
+                        # Getting the elements from the parent, making reference to the json
+                        all_titles = job.find_elements(By.CSS_SELECTOR, elements_path["title_path"])
+                        all_urls = job.find_elements(By.CSS_SELECTOR, elements_path["link_path"])
+                        #all_pubDates = job.find_elements(By.CSS_SELECTOR, elements_path["pubdate_path"])
+                        all_location = job.find_elements(By.CSS_SELECTOR, elements_path["location_path"])
+                        all_category = job.find_elements(By.CSS_SELECTOR, elements_path["description_path"])
+                        for i in all_titles:
+                            title = i.get_attribute("innerHTML")
+                            curated_title = [initial_clean(title)] #We clean the title with the function in handy
+                            total_titles.append(curated_title)
+                        for i in all_urls:
+                            href = i.get_attribute("href")
+                            total_urls.append(href)                        
+                        for i in range(len(all_urls)):
+                            today = date.today()
+                            total_pubdates.append(today)
+                        for i in all_location:
+                            location = i.get_attribute("innerHTML") 
+                            total_locations.append(location)
+                            total_locations.append("NaN")
+                        for i in all_category:
+                            category = i.get_attribute("innerHTML") 
+                            total_descriptions.append(category)
+                        rows = {"title": total_titles, "link": total_urls, "description": total_descriptions, "pubdate": total_pubdates, "location": total_locations}
+        return rows
+
+    #Quit the driver
+    data = elements()
+    driver.quit()
         
-        #The data had missing values so we have to do this to convert it into a pandas df
-        data_dic = dict(data)
-        df = pd.DataFrame.from_dict(data_dic, orient='index')
-        df = df.transpose()
+    #The data had missing values so we have to do this to convert it into a pandas df
+    data_dic = dict(data)
+    df = pd.DataFrame.from_dict(data_dic, orient='index')
+    df = df.transpose()
 
-        #Save it in local machine
-        directory = "./OUTPUTS/"
-        df.to_csv(f"{directory}himalaya.csv", index=False)
-        return df
-
-    def pipeline():
-        df = crawler()
+    #Save it in local machine
+    directory = "./OUTPUTS/"
+    df.to_csv(f"{directory}himalaya.csv", index=False)
+    
+    def pipeline(df):
 
         # Fill missing values with "NaN"
         df.fillna("NaN", inplace=True)
 
-        # Mask to convert minutes|hour% to 1 day ago so it can be parse to date time (cba to say it was posted today, maybe later should be fixed)
-        mask = df['pubdate'].str.contains('minutes|hour%')
-        df.loc[mask, 'pubdate'] = "1 day ago"
-
-        # From relative date strings to date time...
-        def convert_date(x):
-            if isinstance(x, str) and 'ago' in x:
-                return pd.Timestamp.today() - relativedelta(days=int(x.split()[0]))
-            return x
-
         # Convert str to datetime & clean titles
         for col in df.columns:
-            if col == 'pubdate':
-                df[col] = df[col].apply(convert_date).astype('datetime64[ns]')
             if col == 'title':
                 df[col] = df[col].astype(str)
                 df[col] = df[col].apply(clean_rows)
+                df[col] = df[col].str.slice(0, 255)
+            else:
+                df[col] = df[col].str.slice(0, 255)
 
-        ##reindex
-        df = df.reindex(columns=['title', 'link', 'description', 'pubdate', 'location'])
-
-        # Filter rows by a date range (this reduces the number of rows... duh)
-        ## set the date range
-        start_date = pd.to_datetime('2016-01-01')
-        end_date = pd.to_datetime(cut_off)
-        ## apply it
-        date_range_filter = (df['pubdate'] >= start_date) & (df['pubdate'] <= end_date)
-        df = df.loc[~date_range_filter]
 
         # SEND IT TO TO PostgreSQL
-        to_postgre(df)
+        test_postgre(df)
         
         #print the time
         elapsed_time = timeit.default_timer() - start_time
         print("\n")
         print(f"HIMALAYAS is done! all in: {elapsed_time:.2f} seconds.", "\n")
-    pipeline()
+    pipeline(df)
 
 if __name__ == "__main__":
-    himalayas(1, '2023-03-20') 
+    selenium_crawlers(4) 
