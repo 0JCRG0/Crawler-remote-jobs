c397d8b1030d43403bf1006adc45b5cf3c9effda
JCRG <juancarlosrg1999@gmail.com>
2023-04-09 15:09:40 +0100

    fixed selenium_template

--- selenium_template.py
+++ selenium_template.py
@@ -19,172 +19,175 @@ from utils.handy import cleansing_selenium_crawlers, to_postgre, test_postgre, f
 PATH = '/Users/juanreyesgarcia/Library/CloudStorage/OneDrive-FundacionUniversidaddelasAmericasPuebla/DEVELOPER/PROJECTS/CRAWLER_ALL/'
 
 def selenium_crawlers(TYPE):
+    #start timer
+    start_time = timeit.default_timer()
 
-    try:
+    #Modify the options so it is headless - to disable just comment the next 2 lines and use the commented driver
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+        
+    # Start the session
+    driver = webdriver.Firefox(options=options)
 
-        #start timer
-        start_time = timeit.default_timer()
+    """
+    The following is specifying which JSON to load & to which table it will be sent
+    """
 
-        #Modify the options so it is headless - to disable just comment the next 2 lines and use the commented driver
-        options = webdriver.FirefoxOptions()
-        options.add_argument('-headless')
-            
-        # Start the session
-        #driver = webdriver.Firefox()
-        driver = webdriver.Firefox(options=options)
-
-        """
-        The following is specifying which JSON to load & to which table it will be sent
-        """
-
-        if TYPE == 'MAIN':
-            JSON = PATH + '/selenium_resources/main_sel_crawlers.json'
-            POSTGRESQL = to_postgre
-            print("\n", f"Reading {JSON}. Jobs will be sent to PostgreSQL's master_jobs table", "\n")
-            # configure the logger
-            LoggingMasterCrawler()
-        elif TYPE == 'FREELANCE':
-            JSON = PATH + '/selenium_resources/freelance.json'
-            POSTGRESQL = freelance_postgre
-            # configure the logger
-            LoggingFreelanceCrawler()
-            #print("\n", f"Reading {JSON}. Jobs will be sent to PostgreSQL's freelance table", "\n")
-        else:
-            print("\n", "Incorrect argument! Use either 'MAIN' or 'FREELANCE' to run this script.", "\n")
+    if TYPE == 'MAIN':
+        JSON = PATH + '/selenium_resources/main_sel_crawlers.json'
+        POSTGRESQL = to_postgre
+        print("\n", f"Reading {JSON}. Jobs will be sent to PostgreSQL's master_jobs table", "\n")
+        # configure the logger
+        LoggingMasterCrawler()
+    elif TYPE == 'FREELANCE':
+        JSON = PATH + '/selenium_resources/freelance.json'
+        POSTGRESQL = freelance_postgre
+        # configure the logger
+        LoggingFreelanceCrawler()
+        #print("\n", f"Reading {JSON}. Jobs will be sent to PostgreSQL's freelance table", "\n")
+    else:
+        print("\n", "Incorrect argument! Use either 'MAIN' or 'FREELANCE' to run this script.", "\n")
 
 
-        def elements():
-            total_links = []
-            total_titles = []
-            total_pubdates = []
-            total_locations = [] 
-            total_descriptions = []
-            rows = []
+    def elements():
+        total_links = []
+        total_titles = []
+        total_pubdates = []
+        total_locations = [] 
+        total_descriptions = []
+        rows = {"title": total_titles,
+                "link": total_links,
+                "description": total_descriptions,
+                "pubdate": total_pubdates,
+                "location": total_locations}
 
-            with open(JSON) as f:
-                #load the json
-                data = json.load(f)
-                # Access the 'urls' list in the first dictionary of the 'data' list and assign it to the variable 'urls'
-                urls = data[0]["urls"]
-                for url_obj in urls:
-                    #Extract the name of the crawler
-                    name = url_obj['name']
-                    print("\n", f"{name} has started", "\n")
-                    # Extract the 'url' key from the current dictionary and assign it to the variable 'url_prefix'
-                    url_prefix = url_obj['url']
-                    # Extract the first dictionary from the 'elements_path' list in the current dictionary and assign it to the variable 'elements_path'
-                    elements_path = url_obj['elements_path'][0]
-                    #Each site is different to a json file can give us the flexibility we need
-                    ## Extract the "pages_to_crawl"
-                    pages = url_obj['pages_to_crawl']
-                    #Extract the number in which the range is going to start from
-                    start_point = url_obj['start_point']
-                    # You need to +1 because range is exclusive
-                    for i in range(start_point, pages + 1):
-                        #The url from json is incomplete, we need to get the number of the page we are scrapping
-                        ##We do that in the following line
-                        url = url_prefix + str(i)
-                        # get the url
-                        driver.get(url)
-                        print(f"Crawling... {url}")
-                        # establish waiting strategy
-                        driver.implicitly_wait(1)
-                        # GETTING THE PARENT...
-                        jobs = driver.find_elements(By.CSS_SELECTOR, elements_path["jobs_path"])
-                        for job in jobs:
+        #load the json
+        with open(JSON) as f:
+            data = json.load(f)
+            # Access the 'urls' list in the first dictionary of the 'data' list and assign it to the variable 'urls'
+            urls = data[0]["urls"]
+            for url_obj in urls:
+                #Extract the name of the crawler
+                name = url_obj['name']
+                print("\n", f"{name} has started", "\n")
+                # Extract the 'url' key from the current dictionary and assign it to the variable 'url_prefix'
+                url_prefix = url_obj['url']
+                # Extract the first dictionary from the 'elements_path' list in the current dictionary and assign it to the variable 'elements_path'
+                elements_path = url_obj['elements_path'][0]
+                #Each site is different to a json file can give us the flexibility we need
+                ## Extract the "pages_to_crawl"
+                pages = url_obj['pages_to_crawl']
+                #Extract the number in which the range is going to start from
+                start_point = url_obj['start_point']
+                # You need to +1 because range is exclusive
+                for i in range(start_point, pages + 1):
+                    #The url from json is incomplete, we need to get the number of the page we are scrapping
+                    ##We do that in the following line
+                    url = url_prefix + str(i)
+                    # get the url
+                    driver.get(url)
+                    print(f"Crawling... {url}")
+                    # establish waiting strategy
+                    driver.implicitly_wait(1)
+                    # GETTING THE PARENT...
+                    jobs = driver.find_elements(By.CSS_SELECTOR, elements_path["jobs_path"])
+                    for job in jobs:
+                        # create a new dictionary to store the data for the current job
+                        job_data = {}      
+
+                        #TITLES
+                        title_element = job.find_element(By.CSS_SELECTOR, elements_path["title_path"])
+                        if title_element is not None:
+                            title = title_element.get_attribute("innerHTML")
+                            job_data["title"]= title
+                        else:
+                            job_data["title"]= "NaN"
                             
-                            #TITLES
-                            title_element = job.find_element(By.CSS_SELECTOR, elements_path["title_path"])
-                            if title_element is not None:
-                                title = title_element.get_attribute("innerHTML")
-                                total_titles.append(title)
-                            else:
-                                total_titles.append('NaN')
-                            
-                            #LINKS
-                            link_element = job.find_element(By.CSS_SELECTOR, elements_path["link_path"])
-                            if link_element is not None:
-                                href = link_element.get_attribute("href")
-                                total_links.append(href)
-                            else:
-                                total_links.append("NaN")
+                        #LINKS
+                        link_element = job.find_element(By.CSS_SELECTOR, elements_path["link_path"])
+                        if link_element is not None:
+                            href = link_element.get_attribute("href")
+                            job_data["link"]= href
+                        else:
+                            job_data["link"]= "NaN"
 
-                            #PUBDATES - to simplify things & considering this snippet will be run daily datetime is the same day as the day this is running                       
-                            for i in range(len(total_links)):
-                                today = date.today()
-                                total_pubdates.append(today)
+                        #PUBDATES - to simplify things & considering this snippet will be run daily datetime is the same day as the day this is running                       
+                        today = date.today()
+                        job_data["pubdate"] = today
                             
-                            #LOCATIONS
-                            location_element = job.find_element(By.CSS_SELECTOR, elements_path["location_path"])
-                            if location_element is not None:
-                                location = location_element.get_attribute("innerHTML") 
-                                total_locations.append(location)
-                            else:
-                                total_locations.append("NaN")
+                        #LOCATIONS
+                        location_element = job.find_element(By.CSS_SELECTOR, elements_path["location_path"])
+                        if location_element is not None:
+                            location = location_element.get_attribute("innerHTML") 
+                            job_data["location"]= location
+                        else:
+                            job_data["location"]= "NaN"
                             
-                            #Descriptions
-                            description_element = job.find_element(By.CSS_SELECTOR, elements_path["description_path"])
-                            if description_element is not None:
-                                description = description_element.get_attribute("innerHTML") 
-                                total_descriptions.append(description)
-                            else:
-                                total_descriptions.append("NaN")
+                        #Descriptions
+                        description_element = job.find_element(By.CSS_SELECTOR, elements_path["description_path"])
+                        if description_element is not None:
+                            description = description_element.get_attribute("innerHTML") 
+                            job_data["description"]= description
+                        else:
+                            job_data["description"]= "NaN"
                             
-                            #Add them all
-                            rows = {"title": total_titles, "link": total_links, "description": total_descriptions, "pubdate": total_pubdates, "location": total_locations}
-            return rows
+                        # add the data for the current job to the rows list
+                        total_links.append(job_data["link"])
+                        total_titles.append(job_data["title"])
+                        total_pubdates.append(job_data["pubdate"])
+                        total_locations.append(job_data["location"])
+                        total_descriptions.append(job_data["description"])
+        return rows
 
-        #Quit the driver
-        data = elements()
-        driver.quit()
+                    
+    #Quit the driver
+    data = elements()
+    driver.quit()
             
         #The data had missing values so we have to do this to convert it into a pandas df
-        data_dic = dict(data)
-        df = pd.DataFrame.from_dict(data_dic, orient='index')
-        df = df.transpose()
-
+    
+        #data_dic = dict(data)
+        #df = pd.DataFrame.from_dict(data_dic, orient='index')
+        #df = df.transpose()
         #Save it in local machine
-        df.to_csv(PATH + "/OUTPUTS/pre-pipeline-Sel_All.csv", index=False)
+    df = pd.DataFrame(data)
+
+    df.to_csv(PATH + "/OUTPUTS/pre-pipeline-Sel_All.csv", index=False)
 
         # count the number of duplicate rows
-        num_duplicates = df.duplicated().sum()
+    num_duplicates = df.duplicated().sum()
 
             # print the number of duplicate rows
-        print("Number of duplicate rows:", num_duplicates)
+    print("Number of duplicate rows:", num_duplicates)
 
             # remove duplicate rows based on all columns
-        df = df.drop_duplicates()
+    df = df.drop_duplicates()
         
-        def pipeline(df):
+    def pipeline(df):
 
-            # Convert str to datetime & clean titles
-            #TODO: Solve SettingWithCopyWarning
+        # Convert str to datetime & clean titles
+        #TODO: Solve SettingWithCopyWarning
             
-            for col in df.columns:
-                if col != 'pubdate':
-                    df.loc[:, col] = df.loc[:, col].astype(str).apply(cleansing_selenium_crawlers)
+        for col in df.columns:
+            if col != 'pubdate':
+                df.loc[:, col] = df.loc[:, col].astype(str).apply(cleansing_selenium_crawlers)
             
-            #df.loc[:, df.columns != 'pubdate'] = df.loc[:, df.columns != 'pubdate'].astype(str).apply(cleansing_selenium_crawlers)
+        #df.loc[:, df.columns != 'pubdate'] = df.loc[:, df.columns != 'pubdate'].astype(str).apply(cleansing_selenium_crawlers)
 
-            #Save it in local machine
-            df.to_csv(PATH + "/OUTPUTS/post-pipeline-Sel_All.csv", index=False)
+        #Save it in local machine
+        df.to_csv(PATH + "/OUTPUTS/post-pipeline-Sel_All.csv", index=False)
 
-            #Log it 
-            logging.info('Finished Selenium_Crawlers. Results below ⬇︎')
-            # SEND IT TO TO PostgreSQL 
+        #Log it 
+        logging.info('Finished Selenium_Crawlers. Results below ⬇︎')
+        # SEND IT TO TO PostgreSQL 
             
-            POSTGRESQL(df)
+        POSTGRESQL(df)
 
-            #print the time
-            elapsed_time = timeit.default_timer() - start_time
-            print("\n")
-            print(f"The selected selenium crawlers have finished! all in: {elapsed_time:.2f} seconds.", "\n")
-        pipeline(df)
-    
-    except Exception as e:
-        # log the error
-        print(f'Error occurred: {str(e)}')
-        # handle the error
+        #print the time
+        elapsed_time = timeit.default_timer() - start_time
+        print("\n")
+        print(f"The selected selenium crawlers have finished! all in: {elapsed_time:.2f} seconds.", "\n")
+    pipeline(df)
 
 if __name__ == "__main__":
     selenium_crawlers('FREELANCE') 
