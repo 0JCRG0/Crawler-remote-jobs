40295d2073602bcd7a7d45debb9666d0ebbf8bb2
JCRG <juancarlosrg1999@gmail.com>
2023-06-05 10:02:00 -0600

    test

--- api_resources/json_reader.py
+++ api_resources/json_reader.py
@@ -3,19 +3,18 @@ import requests
 import pretty_errors
 
 #Request the api...
-url = 'https://remoteok.com/api'
+url = 'https://cryptojobslist.com/_next/data/SFAWiRBm4m9fe2VR1h4AR/en/tags/remote/all.json?sort=recent&page=1&tag=remote&location=all'
+#url = 'https://remoteok.com/api?api=1'
 
-response = requests.get(url)
+#response = requests.get(url)
+headers = {"User-Agent": "my-app"}
+#headers = {"User-Agent"}
+response = requests.get(url, headers=headers)
 if response.status_code == 200:
     data = json.loads(response.text)
-    
-    test = data[:15]
-    #test_5 = jobs[:9]
-    #print just to test
-    """
+    #data = response.json()
+
     pretty_json = json.dumps(data, indent=4)
     print(pretty_json, type(data))
-    """
-    pretty_json = json.dumps(test, indent=4)
-    print(pretty_json, type(test))
-    
+else:
+    print("aaaa")
