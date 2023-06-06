20db6fde5e6aef980dfd657b1f4686b5ceb5a2cb
JCRG <juancarlosrg1999@gmail.com>
2023-06-05 21:27:06 -0600

    refactor

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import logging
 import timeit
 from api_crawlers import api_template
+from bs4_crawlers import bs4_template
 from rss_crawlers import rss_template
 from selenium_crawlers import selenium_template
 from datetime import date, timedelta
@@ -10,34 +11,28 @@ from utils.handy import LoggingMasterCrawler
 import os
 from dotenv import load_dotenv
 
-""" LOAD ALL THE ENV VARIABLES"""
-#TODO: fix .env
-# API
-load_dotenv('.env')
-PROD_API = os.getenv('PATH_PROD_API', 'DEFAULT')
-TEST_API = os.getenv('PATH_TEST_API', 'DEFAULT')
-
 #SET UP LOGGING
 LoggingMasterCrawler()
 
-def main():
+def main(pipeline):
     # start master timer
     master_start_time = timeit.default_timer()
 
     #Start calling each crawler
     logging.info('ALL CRAWLERS DEPLOYED!')
-    api_template(PROD_API) #1st: PROD_API or TEST_API
-    
-    #Move onto the next one
-    rss_template("MAIN") #1st argument MAIN OR TEST
 
-    #Move onto the next one
-    selenium_template("MAIN") # 'MAIN', TEST or FREELANCE
+    bs4_template(pipeline) #1st argument MAIN OR TEST
+
+    api_template(pipeline) #1st: PROD_API or TEST_API
+    
+    rss_template(pipeline) #1st argument MAIN OR TEST
+
+    selenium_template(pipeline) # 'MAIN', TEST or FREELANCE
 
     #print the time
     elapsed_time = timeit.default_timer() - master_start_time
     logging.info(f'ALL CRAWLERS FINISHED. ALL IN {elapsed_time:.2f} SECONDS!')
 
 if __name__ == "__main__":
-    main()
+    main("TEST")
 
