d140d91b75ae4735e76abdb896f18ef5cb81fa29
JCRG <juancarlosrg1999@gmail.com>
2023-06-08 17:02:30 -0600

    modified crawler

--- other/indeed.py
+++ other/indeed.py
@@ -1,17 +1,29 @@
 #! python3
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
 import re
 import pretty_errors
 import json
 import timeit
-from datetime import date
-from utils.handy import *
+from datetime import date, datetime
+from dotenv import load_dotenv
+import os
+import sys
 
+""" LOAD THE ENVIRONMENT VARIABLES """
 
-#IMPORT THE PATH - YOU NEED TO EXPORT YOUR OWN PATH TO zsh/bash & SAVE IT AS 'CRAWLER_ALL'
-PATH = '/Users/juanreyesgarcia/Library/CloudStorage/OneDrive-FundacionUniversidaddelasAmericasPuebla/DEVELOPER/PROJECTS/CRAWLER_ALL/'
+load_dotenv()
+
+COUNTRY_CODE = os.environ.get('JSON_COUNTRY_CODE', "")
+SAVE_PATH = os.environ.get('SAVE_PATH_INDEED', "")
+UTILS_PATH = os.environ.get('SYS_PATH_APPEND', "")
+
+""" APPEND UTILS' PATH """
+sys.path.append(UTILS_PATH)
+from handy import *
+
 
 
 def indeed(pages, country, keyword):
@@ -29,28 +41,28 @@ def indeed(pages, country, keyword):
     
     # START CRAWLING
     def crawling():
-        total_ids = []
         total_titles = []
         total_links = []
         total_pubdates = []
         total_locations = [] 
         total_descriptions = []
-        rows = {'id': total_ids,
-                'title': total_titles,
+        total_timestamps = []
+        rows = {'title': total_titles,
                 'link': total_links,
                 'description': total_descriptions,
                 'pubdate': total_pubdates,
-                'location': total_locations}
+                'location': total_locations,
+                "timestamp": total_timestamps}
 
 
-        def country_url(i):
+        def country_url(page):
             url = ""
-            with open('./selenium_resources/indeed_country.json') as f:
+            with open(COUNTRY_CODE) as f:
                     data = json.load(f)
                     # Search for a specific code in the JSON document
                     for item in data:
                         if item['code'] == country:  
-                            url= item['url_1'] + keyword + item["url_2"] + str(i)
+                            url= item['url_1'] + keyword + item["url_2"] + str(page)
             return url
 
         for i in range(0, pages * 10, 10):
@@ -58,56 +70,54 @@ def indeed(pages, country, keyword):
             print("\n", f"Crawler deployed on Indeed {country}. Looking for \"{keyword}\" jobs in page number {page}")
             url = country_url(page)
             #Make the request
-            driver.get(url)
-            #Set waiting strategy
-            driver.implicitly_wait(1.5)
-            jobs = driver.find_elements(By.CSS_SELECTOR, '.slider_item.css-kyg8or.eu4oa1w0')    
-            for job in jobs:
-                ##Get the attributes...
+            try:
+                driver.get(url)
+                print(url)
+                #Set waiting strategy
+                driver.implicitly_wait(1.5)
+                jobs = driver.find_elements(By.CSS_SELECTOR, '.slider_item.css-kyg8or.eu4oa1w0')    
+                for job in jobs:
+                    ##Get the attributes...
 
-                job_data = {}
-                
-                #ID
-                id = id_generator(8)
-                job_data["id"] = id
-                #TITLES
-                title_raw = job.find_element(By.CSS_SELECTOR, '[id^="jobTitle"]')
-                if title_raw is not None:
-                    title = title_raw.get_attribute("innerHTML")
-                    job_data["title"]= title
-                else:
-                    job_data["title"]= "NaN"
-                #LINKS
-                link_raw = job.find_element(By.CSS_SELECTOR, '.jcs-JobTitle.css-jspxzf.eu4oa1w0') #THIS FINDS THE PATTERN
-                if link_raw is not None:
-                    link = link_raw.get_attribute("href")
-                    job_data["link"]= link
-                else:
-                    job_data["link"]= "NaN"
-                #DATES
-                today = date.today()
-                job_data["pubdate"] = today
-                #LOCATION
-                location_raw = job.find_element(By.CSS_SELECTOR, '.companyLocation')
-                if location_raw is not None:
-                    location = location_raw.text
-                    job_data["location"]= location
-                else:
-                    job_data["location"]= "NaN"
-                #DESCRIPTIONS
-                description_raw = job.find_element(By.CSS_SELECTOR, '.jobCardShelfContainer.big6_visualChanges .heading6.tapItem-gutter.result-footer')
-                if description_raw is not None:
-                    description = description_raw.get_attribute("innerHTML")
-                    job_data["description"]= description
-                else:
-                    job_data["description"]= "NaN"
-            #Put it all together...
-                total_ids.append(job_data["id"])
-                total_titles.append(job_data["title"])
-                total_links.append(job_data["link"])
-                total_pubdates.append(job_data["pubdate"])
-                total_locations.append(job_data["location"])
-                total_descriptions.append(job_data["description"])
+                    job_data = {}
+                    
+                    title_raw = job.find_element(By.CSS_SELECTOR, '[id^="jobTitle"]')
+                    job_data["title"] = title_raw.get_attribute("innerHTML") if title_raw else "NaN"
+                    
+                    #LINKS
+                    link_raw = job.find_element(By.CSS_SELECTOR, '.jcs-JobTitle.css-jspxzf.eu4oa1w0')
+                    job_data["link"] = link_raw.get_attribute("href") if link_raw else "NaN"
+
+                    #DATES
+                    today = date.today()
+                    job_data["pubdate"] = today
+                    #LOCATION
+                    
+                    location_raw = job.find_element(By.CSS_SELECTOR, '.companyLocation')
+                    job_data["location"] = location_raw.text if location_raw else "NaN"
+
+                    #DESCRIPTIONS
+                    description_raw = job.find_element(By.CSS_SELECTOR, '.jobCardShelfContainer.big6_visualChanges .heading6.tapItem-gutter.result-footer')
+                    job_data["description"] = description_raw.get_attribute("innerHTML") if description_raw else "NaN"
+
+                    #TIMESTAMP
+                    timestamp = datetime.now()
+                    job_data["timestamp"] = timestamp
+
+                    #Put it all together...
+                    total_titles.append(job_data["title"])
+                    total_links.append(job_data["link"])
+                    total_pubdates.append(job_data["pubdate"])
+                    total_locations.append(job_data["location"])
+                    total_descriptions.append(job_data["description"])
+                    total_timestamps.append(job_data["timestamp"])
+            except NoSuchElementException as e:
+                # Handle the specific exception
+                print("Element not found:", e)
+                pass
+            except Exception as e:
+                print("\n", f"WARNING! ELEMENTS NOT FOUND: {e}", "\n")
+                pass
         return rows
     
     data = crawling()
@@ -116,16 +126,22 @@ def indeed(pages, country, keyword):
     #Convert data to a pandas df for further analysis
     df = pd.DataFrame(data)
 
+    df.to_csv(SAVE_PATH, index=False)
+
     #Do some cleaning
     for col in df.columns:
         if col == 'description':
-            df[col] = df[col].apply(indeed_regex)
+            df[col] = df[col].apply(cleansing_selenium_crawlers)
+            #df[col] = ["{MX}".format(i) for i in df[col]]
+        elif col == 'location':
+            df[col] = df[col] + " MX"
+            #TODO: ADD THE CODE_COUNTRY FROM JSON
 
     #Save locally
-    df.to_csv(PATH + '/OUTPUTS/INDEED_MX.csv', index=False)
+    
 
     # SEND IT TO TO PostgreSQL
-    personal_postgre(df)
+    test_postgre(df)
 
     #stop the timer
     elapsed_time = timeit.default_timer() - start_time
@@ -133,7 +149,7 @@ def indeed(pages, country, keyword):
 
 
 if __name__ == "__main__":
-    indeed(2, "MX", "data+analyst") 
+    indeed(1, "MX", "python") 
     """
     1st = number of pages
     2nd = country code from json
