fd151a73d5be3545db50eec9ffe4f80dc74097e2
JCRG <juancarlosrg1999@gmail.com>
2023-06-16 17:07:44 -0600

    new function to follow link

--- utils/FollowLink.py
+++ utils/FollowLink.py
@@ -5,38 +5,31 @@ import bs4
 
 #TODO: FIX REFACTOR -- THERE WAS A KEYERROR WITH DESCRIPTION
 
-def inner_crawler_bs4(followed_link, description_final, inner_link_path, i, description_path_original):
+async def async_follow_link(session, followed_link, description_final, inner_link_tag, default, driver):
 
-    #START SEL
-    options = webdriver.FirefoxOptions()
-    options.add_argument('-headless')
-
-    driver = webdriver.Firefox(options=options)
-
-    link_res = requests.get(followed_link)
-    if link_res.status_code == 200:
-        print(f"""CONNECTION ESTABLISHED ON {followed_link}""", "\n")
-        link_soup = bs4.BeautifulSoup(link_res.text, 'html.parser')
-        description_tag = link_soup.select_one(inner_link_path)
-        if description_tag:
-            description_final = description_tag.text
-            return description_final
+    async with session.get(followed_link) as link_res:
+        if link_res.status == 200:
+            print(f"""CONNECTION ESTABLISHED ON {followed_link}""", "\n")
+            link_text = await link_res.text()
+            link_soup = bs4.BeautifulSoup(link_text, 'html.parser')
+            description_tag = link_soup.select_one(inner_link_tag)
+            if description_tag:
+                description_final = description_tag.text
+                return description_final
+            else:
+                description_final = 'NaN'
+                return description_final
+        elif link_res.status == 403:
+            print(f"""CONNECTION PROHIBITED WITH BS4 ON {followed_link}. STATUS CODE: "{link_res.status}". TRYING WITH SELENIUM""", "\n")
+            driver.get(followed_link)
+            description_tag = driver.find_element(By.CSS_SELECTOR, inner_link_tag)
+            if description_tag:
+                description_final = description_tag.get_attribute("innerHTML")
+                return description_final
+            else:
+                description_final = 'NaN'
+                return description_final
         else:
-            description_final = 'NaN'
+            print(f"""CONNECTION FAILED ON {followed_link}. STATUS CODE: "{link_res.status}". Getting the description from API.""", "\n")
+            description_final = default
             return description_final
-    elif link_res.status_code == 403:
-        print(f"""CONNECTION PROHIBITED WITH BS4 ON {followed_link}. STATUS CODE: "{link_res.status_code}". TRYING WITH SELENIUM""", "\n")
-        driver.get(followed_link)
-        description_tag = driver.find_element(By.CSS_SELECTOR, inner_link_path)
-        if description_tag:
-            description_final = description_tag.get_attribute("innerHTML") 
-            return description_final
-        else:
-            description_final = 'NaN'
-            return description_final
-    else:
-        print(f"""CONNECTION FAILED ON {followed_link}. STATUS CODE: "{link_res.status_code}". Getting the description from initial scraped website.""", "\n")
-        # Get the descriptions & append it to its list
-        description_element = i.select_one(description_path_original)
-        description_final= description_element.text if description_element else "NaN"
-        return description_final
