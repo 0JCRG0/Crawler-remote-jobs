0f5c559fdb8afadbc75574bacd4b3c0fed9d2ecc
JCRG <juancarlosrg1999@gmail.com>
2023-06-19 16:02:31 -0600

    utils followlink

--- utils/FollowLink.py
+++ utils/FollowLink.py
@@ -1,7 +1,23 @@
+from httpcore import TimeoutException
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import bs4
+import asyncio
+from asyncio import TimeoutError
+from concurrent.futures import ThreadPoolExecutor
+import logging
+import os
+
+
+
+async def fetch_sel(url, driver):
+    loop = asyncio.get_event_loop()
+    with ThreadPoolExecutor() as executor:
+        await loop.run_in_executor(executor, driver.get, url)
+    return driver.page_source
 
 #TODO: FIX REFACTOR -- THERE WAS A KEYERROR WITH DESCRIPTION
 
@@ -33,3 +49,65 @@ async def async_follow_link(session, followed_link, description_final, inner_lin
             print(f"""CONNECTION FAILED ON {followed_link}. STATUS CODE: "{link_res.status}". Getting the description from API.""", "\n")
             description_final = default
             return description_final
+
+async def async_follow_link_container(session, followed_link, total_descriptions: list, inner_link_tag, default, driver):
+    async with session.get(followed_link) as link_res:
+        if link_res.status == 200:
+            print(f"""CONNECTION ESTABLISHED ON {followed_link}""", "\n")
+            link_text = await link_res.text()
+            link_soup = bs4.BeautifulSoup(link_text, 'html.parser')
+            description_tag = link_soup.select_one(inner_link_tag)
+            description_inner = description_tag.text if description_tag else "NaN"
+            return total_descriptions.append(description_inner)
+        elif link_res.status == 403:
+            print(f"""CONNECTION PROHIBITED WITH BS4 ON {followed_link}. STATUS CODE: "{link_res.status}". TRYING WITH SELENIUM""", "\n")
+            driver.get(followed_link)
+            description_tag = driver.find_element(By.CSS_SELECTOR, inner_link_tag)
+            description_inner = description_tag.get_attribute("innerHTML") if description_tag else "NaN"
+            return total_descriptions.append(description_inner)
+        else:
+            return total_descriptions.extend(default)
+
+async def async_follow_link_sel(followed_link, description_final, inner_link_tag, default, driver):
+    try:
+        await fetch_sel(followed_link, driver)
+        print(f"""CONNECTION ESTABLISHED ON {followed_link}""", "\n")
+        # Set up a WebDriverWait instance with a timeout of 10 seconds
+        logging.warning(f"Current URL: {driver.current_url}")
+        logging.warning(f"Page source: {driver.page_source}")
+        wait = WebDriverWait(driver, 10)
+        # Wait for the element to be present in the DOM and to be visible
+        description_tag = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, inner_link_tag)))
+        #await asyncio.sleep(5)  # Wait for 5 seconds
+        #description_tag = driver.find_element(By.CSS_SELECTOR, inner_link_tag)
+        description_final = description_tag.get_attribute("innerHTML") if description_tag else "NaN"
+        return description_final
+    except asyncio.TimeoutError:
+        print("Element not found within the specified wait time.")
+        description_final = "NaN"
+        return description_final
+    
+    except Exception as e:
+        print(f"""CONNECTION PROHIBITED WITH SELENIUM ON {followed_link}. ERROR: {str(e)}""", "\n")
+        description_final = default
+        return description_final
+
+async def async_follow_link_container_sel(followed_link, total_descriptions: list, inner_link_tag, default, driver, fetch):
+    try:
+        wait = WebDriverWait(driver, 10)
+        await fetch(followed_link, driver)
+        print(f"""CONNECTION ESTABLISHED ON {followed_link}""", "\n")
+        description_tag = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, inner_link_tag)))
+        description_inner = description_tag.get_attribute("innerHTML") if description_tag else "NaN"
+        # Wait for the element to be present in the DOM and to be visible
+        try:
+            description_tag = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, inner_link_tag)))
+            description_inner = description_tag.get_attribute("innerHTML") if description_tag else "NaN"
+            return total_descriptions.append(description_inner)
+        except TimeoutException:
+            print("Element not found within the specified wait time.")
+            description_inner = "NaN"
+            return total_descriptions.append(description_inner)
+    except Exception as e:
+        print(f"""CONNECTION PROHIBITED WITH SELENIUM ON {followed_link}. ERROR: {str(e)}""", "\n")
+        return total_descriptions.extend(default)
