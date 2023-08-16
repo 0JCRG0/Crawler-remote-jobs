13db2ae7446a4c8b89fa7f9c7003dfaf97c43010
JCRG <juancarlosrg1999@gmail.com>
2023-08-16 17:58:17 -0600

    cleaning function

--- utils/indeed_utils.py
+++ utils/indeed_utils.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from sql.clean_loc import clean_location_rows
+from sql.clean_loc import clean_location_rows, convert_names_to_codes
 from utils.handy import *
 
 def clean_postgre_indeed(df, S, Q, c_code):
@@ -12,6 +12,8 @@ def clean_postgre_indeed(df, S, Q, c_code):
 			#df[col] = ["{MX}".format(i) for i in df[col]]
 		elif col == 'location':
 			df[col] = df[col] + " " + str(c_code)
+			df[col] = df[col].apply(clean_location_rows)
+			df[col] = df[col].apply(convert_names_to_codes)
 	
 	logging.info('Finished Indeed. Results below ⬇︎')
 
