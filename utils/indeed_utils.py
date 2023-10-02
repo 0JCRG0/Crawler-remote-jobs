3a777264d2ddc98b6e564f1290a46f6ac9f89194
JCRG <juancarlosrg1999@gmail.com>
2023-10-02 12:24:23 -0600

    fixed bugs

--- utils/indeed_utils.py
+++ utils/indeed_utils.py
@@ -12,8 +12,16 @@ def clean_postgre_indeed(df, S, Q, c_code):
 			#df[col] = ["{MX}".format(i) for i in df[col]]
 		elif col == 'location':
 			df[col] = df[col] + " " + str(c_code)
-			df[col] = df[col].apply(clean_location_rows)
-			df[col] = df[col].apply(convert_names_to_codes)
+			df[col] = df[col].str.replace(r'[{}[\]\'",]', '', regex=True)
+			df[col] = df[col].str.replace(r'\b(\w+)\s+\1\b', r'\1', regex=True) # Removes repeated words
+			df[col] = df[col].str.replace(r'\d{4}-\d{2}-\d{2}', '', regex=True)  # Remove dates in the format "YYYY-MM-DD"
+			df[col] = df[col].str.replace(r'(USD|GBP)\d+-\d+/yr', '', regex=True)  # Remove USD\d+-\d+/yr or GBP\d+-\d+/yr.
+			df[col] = df[col].str.replace('[-/]', ' ', regex=True)  # Remove -
+			df[col] = df[col].str.replace(r'(?<=[a-z])(?=[A-Z])', ' ', regex=True)  # Insert space between lowercase and uppercase letters
+			pattern = r'(?i)\bRemote Job\b|\bRemote Work\b|\bRemote Office\b|\bRemote Global\b|\bRemote with frequent travel\b'     # Define a regex patter for all outliers that use remote 
+			df[col] = df[col].str.replace(pattern, 'Worldwide', regex=True)
+			df[col] = df[col].replace('(?i)^remote$', 'Worldwide', regex=True) # Replace 
+			df[col] = df[col].str.strip()  # Remove trailing white space
 	
 	logging.info('Finished Indeed. Results below ⬇︎')
 
