89ccae6ae09944dcfda356e73c49f951d9f5b74c
JCRG <juancarlosrg1999@gmail.com>
2023-10-02 12:23:34 -0600

    fixed bugs

--- utils/bs4_utils.py
+++ utils/bs4_utils.py
@@ -5,7 +5,7 @@ from utils.handy import *
 
 
 
-def clean_postgre_bs4(df, S, Q):
+def clean_postgre_bs4(df: pd.DataFrame, S, Q):
 	#df = pd.DataFrame(data) # type: ignore
 	"""data_dic = dict(data) # type: ignore
 	df = pd.DataFrame.from_dict(data_dic, orient='index')
@@ -27,15 +27,16 @@ def clean_postgre_bs4(df, S, Q):
 			newvals = df.loc[:, col].astype(str).apply(cleansing_selenium_crawlers)
 			df[df.columns[i]] = newvals
 		elif col == 'location':
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(cleansing_selenium_crawlers)
-			df[df.columns[i]] = newvals
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(clean_location_rows)
-			df[df.columns[i]] = newvals
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(convert_names_to_codes)
-			df[df.columns[i]] = newvals
+			df[col] = df[col].str.replace(r'[{}[\]\'",]', '', regex=True)
+			df[col] = df[col].str.replace(r'\b(\w+)\s+\1\b', r'\1', regex=True) # Removes repeated words
+			df[col] = df[col].str.replace(r'\d{4}-\d{2}-\d{2}', '', regex=True)  # Remove dates in the format "YYYY-MM-DD"
+			df[col] = df[col].str.replace(r'(USD|GBP)\d+-\d+/yr', '', regex=True)  # Remove USD\d+-\d+/yr or GBP\d+-\d+/yr.
+			df[col] = df[col].str.replace('[-/]', ' ', regex=True)  # Remove -
+			df[col] = df[col].str.replace(r'(?<=[a-z])(?=[A-Z])', ' ', regex=True)  # Insert space between lowercase and uppercase letters
+			pattern = r'(?i)\bRemote Job\b|\bRemote Work\b|\bRemote Office\b|\bRemote Global\b|\bRemote with frequent travel\b'     # Define a regex patter for all outliers that use remote 
+			df[col] = df[col].str.replace(pattern, 'Worldwide', regex=True)
+			df[col] = df[col].replace('(?i)^remote$', 'Worldwide', regex=True) # Replace 
+			df[col] = df[col].str.strip()  # Remove trailing white space
 
 		
 		#Save it in local machine
