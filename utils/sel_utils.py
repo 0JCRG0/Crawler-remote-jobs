a39a5664b5f3b36a4f0736e2540c3ae87adc7ab2
JCRG <juancarlosrg1999@gmail.com>
2023-10-02 12:24:36 -0600

    fixed bugs

--- utils/sel_utils.py
+++ utils/sel_utils.py
@@ -10,19 +10,19 @@ def clean_postgre_sel(df, csv_path, db):
 	
 	""" CLEANING AVOIDING DEPRECATION WARNING """
 	for col in df.columns:
-		if col != 'pubdate':
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(cleansing_selenium_crawlers)
-			df[df.columns[i]] = newvals
-	
-	for col in df.columns:
-		if col == 'location':
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(clean_location_rows)
-			df[df.columns[i]] = newvals
-			i = df.columns.get_loc(col)
-			newvals = df.loc[:, col].astype(str).apply(convert_names_to_codes)
-			df[df.columns[i]] = newvals
+		if col == 'description':
+			df[col] = df[col].str.replace(r'[{}[\]\'",]', '', regex=True)
+		elif col == 'location':
+			df[col] = df[col].str.replace(r'[{}[\]\'",]', '', regex=True)
+			df[col] = df[col].str.replace(r'\b(\w+)\s+\1\b', r'\1', regex=True) # Removes repeated words
+			df[col] = df[col].str.replace(r'\d{4}-\d{2}-\d{2}', '', regex=True)  # Remove dates in the format "YYYY-MM-DD"
+			df[col] = df[col].str.replace(r'(USD|GBP)\d+-\d+/yr', '', regex=True)  # Remove USD\d+-\d+/yr or GBP\d+-\d+/yr.
+			df[col] = df[col].str.replace('[-/]', ' ', regex=True)  # Remove -
+			df[col] = df[col].str.replace(r'(?<=[a-z])(?=[A-Z])', ' ', regex=True)  # Insert space between lowercase and uppercase letters
+			pattern = r'(?i)\bRemote Job\b|\bRemote Work\b|\bRemote Office\b|\bRemote Global\b|\bRemote with frequent travel\b'     # Define a regex patter for all outliers that use remote 
+			df[col] = df[col].str.replace(pattern, 'Worldwide', regex=True)
+			df[col] = df[col].replace('(?i)^remote$', 'Worldwide', regex=True) # Replace 
+			df[col] = df[col].str.strip()  # Remove trailing white space
 		
 	#Save it in local machine
 	df.to_csv(csv_path, index=False)
